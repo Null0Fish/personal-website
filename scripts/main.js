0dd0982f86cb8b837b5c0ef3c3942ef1dd7189bc
Jackson <jacksonconnors@hotmail.com>
2025-10-29 12:25:57 -0400

    reformated projects to work in json and added content to about page

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const galleryGrid = document.querySelector(".sample-gallery-grid");
+
+    fetch("projects.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+
+            const projects = data.projects.slice(0, 3);
+            galleryGrid.innerHTML = "";
+
+            projects.forEach(project => {
+                const projectCard = document.createElement("a");
+                projectCard.href = "/projects.html#$gallery";
+                console.log(projectCard.href)
+                projectCard.classList.add("sample-project");
+                
+                projectCard.innerHTML = `
+                <img src="${project.preview_image}" alt="${project.alt}">
+                <div class="project-info">
+                    <h3>${project.title}</h3>
+                    <p>${project.preview_description}</p>
+                </div>
+                `;
+
+                galleryGrid.appendChild(projectCard);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
---
+++ scripts/projects.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("gallery");
+  
+    fetch("projects.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            data.projects.forEach((project, index) => {
+                const projectDiv = document.createElement("div");
+                projectDiv.classList.add("project");
+                if (index % 2 === 1) {
+                  projectDiv.innerHTML = `
+                  <img src="${project.image}" alt="${project.alt}">
+                  <div class="project-text">
+                    <h3>${project.title}</h3>
+                    <p>${project.description}</p>
+                    <small>${project.date}</small>
+                  </div>
+                  `;
+                } else {
+                  projectDiv.innerHTML = `
+                  <div class="project-text">
+                    <h3>${project.title}</h3>
+                    <p>${project.description}</p>
+                    <small>${project.date}</small>
+                  </div>
+                  <img src="${project.image}" alt="${project.alt}">
+                `;
+                }
+
+                container.appendChild(projectDiv);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
