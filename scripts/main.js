f69f629089143adf2a12a9e9230bd2348616724e
Jackson <jacksonconnors@hotmail.com>
2025-12-07 20:25:20 -0500

    Commited all filas needed for site to pull projects from git

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const galleryGrid = document.querySelector(".sample-gallery-grid");
+    const galleryGrid = document.querySelector(".gallery-grid");
 
-    fetch("projects.json")
-        .then(response => {
-            return response.json();
-        })
+    fetch("../project_files/projects.json")
+        .then(response => response.json())
         .then(data => {
-
-            const projects = data.projects.slice(0, 3);
+            const projects = Object.values(data);
             galleryGrid.innerHTML = "";
 
             projects.forEach(project => {
                 const projectCard = document.createElement("a");
-                projectCard.href = "#";
-                console.log(projectCard.href)
-                projectCard.classList.add("sample-project");
-                // Uses the default template after reading data
+                projectCard.href = project.github_url;
+                projectCard.target = "_blank";
+                projectCard.classList.add("project");
+
                 projectCard.innerHTML = `
-                <img src="${project.preview_image}" alt="${project.alt}">
-                <div class="project-info">
-                    <h3>${project.title}</h3>
-                    <p>${project.preview_description}</p>
-                </div>
+                    <img src="${project.image}" alt="${project.image_alt}">
+                    <div class="project-info">
+                        <h3>${project.title}</h3>
+                        <p>${project.description}</p>
+                    </div>
                 `;
 
                 galleryGrid.appendChild(projectCard);
             });
-        })
-        .catch(error => {
-            console.error(error);
         });
+    console.log(galleryGrid)
 });
